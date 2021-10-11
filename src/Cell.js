7bdf5ffbf455dc06b1e2f48afdf77dc49b2daf81
Jess Pesale <jess_pesale@aol.com>
2021-10-11 16:41:15 -0400

    commit tree 22cbf3431276eb117608a39cb62730d2d3dfab6e

---
+++ src/Cell.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default class Cell extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {color: props.value}
+    }
+
+    handleClick = () => {
+        console.log(this.state.color)
+        this.setState({
+            color: '#333'
+        })
+    }
+
+    render() {
+        return(
+            <div 
+            className="cell" 
+            style={{backgroundColor: this.state.color}} 
+            onClick={this.handleClick}
+            >
+            </div>
+        )
+    }
+
+}
+
+
